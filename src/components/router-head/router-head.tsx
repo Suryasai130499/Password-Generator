c6cbb18cffd81872fb17a85136a041feb6600003
Surya <surya@thesilverlabs.com>
2022-10-14 11:28:08 +0530

    Removed boiler plate

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,9 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
 
-/**
- * The RouterHead component is placed inside of the document `<head>` element.
- */
 export const RouterHead = component$(() => {
   const head = useDocumentHead();
   const loc = useLocation();
@@ -19,7 +16,7 @@ export const RouterHead = component$(() => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Poppins&amp;display=swap"
+        href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap"
         rel="stylesheet"
       />
 
